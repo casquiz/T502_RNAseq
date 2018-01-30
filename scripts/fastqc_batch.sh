92bd957f374d709b3492f295e513e1bd29ae9e64
Alex <35581804+casquiz@users.noreply.github.com>
2018-01-30 16:50:36 -0500

    Update fastqc_batch.sh

--- scripts/fastqc_batch.sh
+++ scripts/fastqc_batch.sh
@@ -10,27 +10,27 @@ module load fastqc
 
 fileDir=/N/dc2/scratch/rtraborn/T502_fastqs/PP_RNAseq
 ####### Before running the script, please enter path to desired output directory, below ####
-fqDir=<provide path to desired output directory>
+fqDir=/Users/alexandercampbell/desktop
 
 cd $fqDir
 
-ln -s ${fileDir}/GSF1659-NHR40-1_S5_R1_001.fastq.gz NHR40-1.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-1_S5_R2_001.fastq.gz NHR40-1.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-2_S6_R1_001.fastq.gz NHR40-2.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-2_S6_R2_001.fastq.gz NHR40-2.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-3_S7_R1_001.fastq.gz NHR40-3.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-3_S7_R2_001.fastq.gz NHR40-3.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-4_S8_R1_001.fastq.gz NHR40-4.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-NHR40-4_S8_R2_001.fastq.gz NHR40-4.R2.fastq.gz
+ln -s ${fileDir}/GSF1659-EC-7_S17_R1_001.fastq.gz
+ln -s ${fileDir}/GSF1659-EC-7_S17_R2_001.fastq.gz
+ln -s ${fileDir}/GSF1659-ECL-1_S15_R1_001.fastq.gz
+ln -s ${fileDir}/GSF1659-ECL-1_S15_R2_001.fastq.gz
+ln -s ${fileDir}/GSF1659-ECL-4_S16_R1_001.fastq.gz
+ln -s ${fileDir}/GSF1659-ECL-4_S16_R2_001.fastq.gz
+ln -s ${fileDir}/GSF1659-KP-84_S12_R2_001.fastq.gz
+ln -s ${fileDir}/GSF1659-KP-84_S12_R1_001.fastq.gz
 
-ln -s ${fileDir}/GSF1659-Seud1-1_S1_R1_001.fastq.gz Seud1-1.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-1_S1_R2_001.fastq.gz Seud1-1.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-2_S2_R1_001.fastq.gz Seud1-2.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-2_S2_R2_001.fastq.gz Sedu1-2.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-3_S3_R1_001.fastq.gz Seud1-3.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-3_S3_R2_001.fastq.gz Seud1-3.R2.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-4_S4_R1_001.fastq.gz Seud1-4.R1.fastq.gz
-ln -s ${fileDir}/GSF1659-Seud1-4_S4_R2_001.fastq.gz Seud1-4.R2.fastq.gz
+ln -s ${fileDir}GSF1659-KP-49_S9_R1_001.fastq.gz
+ln -s ${fileDir}/GSF1659-KP-49_S9_R2_001.fastq.gz
+ln -s ${fileDir}/GSF1659-KP-80_S10_R1_001.fastq.gz
+ln -s ${fileDir}/GSF1659-KP-80_S10_R2_001.fastq.gz
+ln -s ${fileDir}/GSF1659-KP-83_S11_R1_001.fastq.gz
+ln -s ${fileDir}/GSF1659-KP-83_S11_R2_001.fastq.gz
+ln -s ${fileDir}/GSF1659-KP-85_S13_R1_001.fastq.gz
+ln -s ${fileDir}/GSF1659-KP-85_S13_R2_001.fastq.gz
 
 echo "Starting job"
 
